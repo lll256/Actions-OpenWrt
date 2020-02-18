3b653e501f438d978a3084681ce0c71fac2cb250
lll256 <37477939+lll256@users.noreply.github.com>
2020-02-18 15:45:21 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,6 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+src-git CTCGFW https://github.com/project-openwrt/openwrt
+./scripts/feeds update -a
+./scripts/feeds install -a
