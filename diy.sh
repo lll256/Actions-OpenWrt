20a15adfdcc69de3fdfe9808876cc786647d8bd8
lll256 <37477939+lll256@users.noreply.github.com>
2020-02-22 15:09:06 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,4 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/lll256/lll256 package/lll256
+./scripts/feeds update -a
+./scripts/feeds install -a
 
